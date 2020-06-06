4a37bd8a6aebbedd7674c1a92449a13b3bceeb73
eroatta <emilianoroatta@gmail.com>
2020-06-06 15:15:51 -0300

    initial app setup

---
+++ README.md
@@ -0,0 +1,19 @@
+# diff-viewer
+
+**diff-viewer** allows us to visualize the differences between two versions of a file, in GitHub-like style. It's focused on comparing the current version against the post-processed version, created by [src_reader](https://github.com/eroatta/src-reader).
+
+## Usage
+
+_TBD_
+
+## Screenshots
+
+_TBD_
+
+## Inspiration
+
+Adapted from the examples on [react-gh-like-diff](https://github.com/neighborhood999/react-gh-like-diff).
+
+## License
+
+See the [LICENSE](LICENSE) file for license rights and limitations (MIT).
---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import { ReactGhLikeDiff } from "react-gh-like-diff";
+import fetch from "unfetch";
+import "react-gh-like-diff/dist/css/diff2html.min.css";
+
+class App extends Component {
+  state = {
+    past: "",
+    current: ""
+  };
+
+  url = window.location.pathname;
+  title = this.url.replace("/rewrites/", "");
+
+  separator = this.title.indexOf("/", this.title.indexOf("/")+1);
+  repo = this.title.substr(0, this.separator);
+  file = this.title.substr(this.separator+1);
+
+  past = "https://raw.githubusercontent.com/" + this.repo + "/v1.2/" + this.file;
+  current = "https://raw.githubusercontent.com/" + this.repo + "/master/" + this.file;
+
+  componentDidMount() {
+    fetch(this.past)
+      .then(response => response.text())
+      .then(past => this.setState({ past }));
+
+    fetch(this.current)
+      .then(response => response.text())
+      .then(current => this.setState({ current }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <ReactGhLikeDiff
+          options={{
+            originalFileName: this.title,
+            updatedFileName: this.title
+          }}
+          past={this.state.past}
+          current={this.state.current}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
